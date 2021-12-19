d609ab29aab8c68a9f1b4cfff31449cb9a2463d7
bakkaSansanBakka <polina.potapeyko@mail.ru>
2021-12-19 16:52:22 +0300

    add website scripts

---
+++ scripts/ImageReplace.js
@@ -0,0 +1,37 @@
+const gasTubeToImage = () => {
+  document.getElementById("gasTube").innerHTML =
+    "<img src='../resources/gaz tube.jpg' />";
+};
+
+const imageToGasTube = () => {
+  document.getElementById("gasTube").innerHTML =
+    "<span>газоотводной трубки</span>";
+};
+
+const rackToImage = () => {
+  document.getElementById("rack").innerHTML =
+    "<img src='../resources/rack.jpg' />";
+};
+
+const imageToRack = () => {
+  document.getElementById("rack").innerHTML =
+    "<span>лабораторного штатива с зажимом</span>";
+};
+
+const testTubeToImage = () => {
+  document.getElementById("test-tube").innerHTML =
+    "<img src='../resources/test-tube.jpg' />";
+};
+
+const imageToTestTube = () => {
+  document.getElementById("test-tube").innerHTML = "<span>пробирок</span>";
+};
+
+const spiritlampToImage = () => {
+  document.getElementById("spiritlamp").innerHTML =
+    "<img src='../resources/spiritlamp.jpg' />";
+};
+
+const imageToSpiritlamp = () => {
+  document.getElementById("spiritlamp").innerHTML = "<span>спиртовки</span>";
+};
